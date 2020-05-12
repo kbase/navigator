e08e3db6318b54fb0cad11a4700d01a5aac957cd
Bill Riehl <wjriehl@lbl.gov>
2020-05-11 17:30:05 -0700

    remake data icons and decompose data view

---
+++ src/client/components/dashboard/NarrativeList/DataView.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { DataObject } from '../../../utils/narrativeData';
+import { getWSTypeName } from '../../../utils/stringUtils';
+
+interface Props {
+    dataObjects: Array<DataObject>
+}
+
+// interface DataObject {
+//     readableType: string;
+//     obj_type: string;
+//     name: string;
+// }
+
+
+export default function DataView(props: Props) {
+    const rows = props.dataObjects
+        .slice(0, 50)
+        .map(obj => {
+            obj.readableType = getWSTypeName(obj.obj_type);
+            return obj;
+        })
+        .sort((a, b) => a.readableType.localeCompare(b.readableType))
+        .map(obj => dataViewRow(obj));
+    return (
+        <div>
+            <p className="black-60">
+                {props.dataObjects.length} total objects in the Narrative:
+            </p>
+            <div className="dt dt--fixed">{rows}</div>
+        </div>
+    );
+
+    // View for each row in the data listing for the narrative
+    function dataViewRow(obj: DataObject) {
+        const key = obj.name + obj.obj_type;
+        const leftWidth = 40; // percentage
+        return (
+        <div key={key} className="dt-row">
+            <span
+            className="dib mr2 dtc b pa2 truncate"
+            style={{ width: leftWidth + '%' }}
+            >
+            <i className="fa fa-database dib mr2 green"></i>
+            {obj.readableType}
+            </span>
+            <span
+            className="dib dtc pa2 black-60 truncate"
+            style={{ width: 100 - leftWidth + '%' }}
+            >
+            {obj.name}
+            </span>
+        </div>
+        );
+    }
+
+}
---
+++ src/client/components/generic/Icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * Generates various KBase Narrative icons from input props.
+ */
+
+interface TypeProps {
+
+}
+
+interface CellProps {
+
+}
+
+export function TypeIcon(props: TypeProps) {
+
+}
+
+export function CellIcon(props: CellProps) {
+
+}
