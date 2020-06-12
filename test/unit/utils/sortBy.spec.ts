8b3b34afaba70fc52c3a635b0eb52c8847a283c2
Bill Riehl <wjriehl@lbl.gov>
2020-06-12 09:43:54 -0700

    more tests

--- test/unit/utils/sortBy.spec.ts
+++ test/unit/utils/sortBy.spec.ts
@@ -5,3 +5,16 @@ test('sortBy should work for a simple number array', () => {
     const ordered = [ 1, 2, 3, 4, 5, 6 ];
     expect(sortBy(disordered, v => v)).toEqual(ordered);
 });
+
+test('sortBy should not alter a flat number array', () => {
+    const arr = [1, 1, 1, 1, 1, 1];
+    expect(sortBy(arr, v => v)).toEqual(arr);
+});
+
+test('sortBy should work against a complex object array', () => {
+    const objArr = [{ a: { b: 1 } }, {a: { b: -5 } }, {a: { b: 2 } } ];
+    const sorted = [{ a: { b: -5 } }, {a: { b: 1 } }, {a: { b: 2 } } ];
+
+    expect(sortBy(objArr, v => v.a.b)).toEqual(sorted);
+
+});
