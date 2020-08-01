1175b732b790fd31242525edd5de8c48e0fefd27
Bill Riehl <wjriehl@lbl.gov>
2020-08-01 08:02:19 -0700

    prettier

--- src/client/components/generic/__tests__/FilterDropdown.spec.tsx
+++ src/client/components/generic/__tests__/FilterDropdown.spec.tsx
@@ -2,7 +2,6 @@
  * @jest-environment jsdom
  */
 
-
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import { FilterDropdown } from '../FilterDropdown';
@@ -31,35 +30,37 @@ test('Filterdropdown should expand and collapse as expected', () => {
 });
 
 test('FilterDropdown should start with showing its options with the isOpen prop', () => {
-    const dropdown = mount(
-        <FilterDropdown
-            onSelect={(idx, val) => {}}
-            txt={'text'}
-            items={['item1', 'item2']}
-            isOpen={true}
-        />
-    );
-    expect(dropdown.find('div.shadow-3').children().length).toEqual(2);
+  const dropdown = mount(
+    <FilterDropdown
+      onSelect={(idx, val) => {}}
+      txt={'text'}
+      items={['item1', 'item2']}
+      isOpen={true}
+    />
+  );
+  expect(dropdown.find('div.shadow-3').children().length).toEqual(2);
 });
 
 test('FilterDropdown should close on document click', () => {
-    const wrapper = mount(
-        <FilterDropdown
-        onSelect={(idx, val) => {}}
-        txt={'text'}
-        items={['item1', 'item2']}
-        isOpen={true}
-        />
-    );
-    expect(wrapper.find('div.shadow-3').children().length).toEqual(2);
+  const wrapper = mount(
+    <FilterDropdown
+      onSelect={(idx, val) => {}}
+      txt={'text'}
+      items={['item1', 'item2']}
+      isOpen={true}
+    />
+  );
+  expect(wrapper.find('div.shadow-3').children().length).toEqual(2);
 
-    /* Noting here that this kinda skirts the outside of Enzyme's simulation.
-     * This click event does trigger the events set up in FilterDropdown
-     * but not through Enzyme, so it doesn't change the structure that Enzyme
-     * knows about, but it DOES change the rendered HTML.
-     * There's probably a better way to do this, involved with rewriting how
-     * that listener gets created, but I don't know what it is, and this works.
-     */
-    document.body.click();
-    expect(wrapper.find(FilterDropdown).html()).toEqual('<div class="dib relative"><a class="dim dib pa2 br2 ba b--solid b--black-20 pointer bg-white">text: item1<i class="ml1 fa fa-caret-down"></i></a></div>');
+  /* Noting here that this kinda skirts the outside of Enzyme's simulation.
+   * This click event does trigger the events set up in FilterDropdown
+   * but not through Enzyme, so it doesn't change the structure that Enzyme
+   * knows about, but it DOES change the rendered HTML.
+   * There's probably a better way to do this, involved with rewriting how
+   * that listener gets created, but I don't know what it is, and this works.
+   */
+  document.body.click();
+  expect(wrapper.find(FilterDropdown).html()).toEqual(
+    '<div class="dib relative"><a class="dim dib pa2 br2 ba b--solid b--black-20 pointer bg-white">text: item1<i class="ml1 fa fa-caret-down"></i></a></div>'
+  );
 });
