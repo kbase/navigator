d70326bf43adcb11e286f61aac5f5de6f757c5f1
Erik Pearson <eapearson@lbl.gov>
2021-02-04 08:24:24 -0800

    loading spinner supports a custom message

--- src/client/components/generic/LoadingSpinner.tsx
+++ src/client/components/generic/LoadingSpinner.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 interface Props {
-  loading: boolean;
+    loading: boolean;
+    message?: string;
 }
 
-// Loading spinner
 export function LoadingSpinner(props: Props) {
-  if (!props.loading) {
-    return <></>;
-  }
-  return (
-    <p className="black-60 mt3">
-      <i className="fa fa-gear fa-spin"></i> Loading...
-    </p>
-  );
+    if (!props.loading) {
+        return <></>;
+    }
+
+    return (
+        <p className="black-60 mt3">
+            <i className="fa fa-gear fa-spin"></i> {props.message || 'Loading...'}
+        </p>
+    );
 }
