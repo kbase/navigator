0c8cb5e8d78b31f85259fd34c1bdfc9f9da8c3a0
Erik Pearson <eapearson@lbl.gov>
2021-08-26 07:29:00 -0700

    refactor LinkOrgItem to extract Org selector, extract some data access code, improve async process management

--- src/client/components/dashboard/NarrativeList/ControlMenu/__tests__/LinkOrgItem.spec.tsx
+++ src/client/components/dashboard/NarrativeList/ControlMenu/__tests__/LinkOrgItem.spec.tsx
@@ -4,12 +4,60 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+import { enableFetchMocks } from 'jest-fetch-mock';
 
 import LinkOrgItem from '../LinkOrgItem';
 import { Doc } from '../../../../../utils/narrativeData';
 
-describe('Preview tests', () => {
-  it('<LinkOrgItem> should render', () => {
+enableFetchMocks();
+
+describe('LinkOrgItem', () => {
+  it('with no no member groups should render', () => {
+    // Note I added the mocking below to make this test pass, but using empty
+    // mock data only exercises that specific case.
+    window._env.username = 'foo';
+    window._env.token = 'bar';
+    fetchMock.mockIf('https://ci.kbase.us/services', async (req: Request) => {
+      console.log('mock 1');
+      if (req.url.includes('/groups/')) {
+        console.log('mock 2');
+        if (req.url.endsWith('/group')) {
+          return Promise.resolve({
+            body: JSON.stringify([]),
+            status: 200,
+          });
+        } else if (req.url.endsWith('/member')) {
+          return Promise.resolve({
+            body: JSON.stringify([]),
+          });
+        }
+      } else if (req.url.endsWith('/ws')) {
+        console.log('mock ws');
+
+        return {
+          body: JSON.stringify({
+            version: '1.1',
+            result: [
+              {
+                perms: [
+                  {
+                    foo: 'w',
+                  },
+                ],
+              },
+            ],
+          }),
+        };
+      }
+      throw new Error('Not handled');
+    });
+    /*
+    Note that the 'foo' in the perms above needs to match the test config user.
+    The user used in getCurrentUserPermission is that returend by Runtime.username(),
+    which matches Runtime.token(). .username() actually gets it's value from
+    window._env.username (_env appears to be used as a global namespace !?).
+    getToken() simply takes the value of the cookie 'kbase_session'
+    */
     const narrative: Doc = {
       access_group: 1,
       cells: [],
