c9854d1aeaeefa5a8560e96e84a6d8050b9f7625
Erik Pearson <eapearson@lbl.gov>
2021-08-26 07:26:27 -0700

    loading spinner test should not assume implemented as <i> - it doesn't matter; improve tests

--- src/client/components/generic/__tests__/LoadingSpinner.spec.tsx
+++ src/client/components/generic/__tests__/LoadingSpinner.spec.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoadingSpinner } from '../LoadingSpinner';
 
-test('Loading spinner returns an empty element if loading is false', () => {
-  const elem = shallow(<LoadingSpinner loading={false} />);
-  expect(elem.html()).toBeFalsy(); // should be an empty string or null, whatever the renderer wants
-});
+describe('The LoadingSpinner component', () => {
+  test('should create an empty element if loading is false', () => {
+    const elem = shallow(<LoadingSpinner loading={false} />);
+    expect(elem.html()).toEqual('');
+  });
 
-test('Loading spinner should render correctly when loading', () => {
-  const elem = shallow(<LoadingSpinner loading={true} />);
-  expect(elem.find('i').hasClass('fa fa-gear fa-spin')).toBeTruthy();
+  test('should render the spinner icon when loading', () => {
+    const elem = shallow(<LoadingSpinner loading={true} />);
+    expect(elem.exists('.fa.fa-gear.fa-spin')).toBeTruthy();
+  });
+
+  test('should render the default message when loading', () => {
+    const elem = shallow(<LoadingSpinner loading={true} />);
+    expect(elem.text()).toContain('Loading');
+  });
+
+  test('should render the custom message when loading', () => {
+    const elem = shallow(<LoadingSpinner loading={true} message="Foo bar" />);
+    expect(elem.text()).toContain('Foo bar');
+  });
 });
