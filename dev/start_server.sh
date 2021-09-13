0b9e5ca4db2959a14dce09f688de58ea22d6a358
Erik Pearson <eapearson@lbl.gov>
2021-09-13 16:16:05 -0700

    enhance script to create docker network if need be

--- dev/start_server.sh
+++ dev/start_server.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# Start the development servers
-
 set -e
 
 export DEVELOPMENT=1
-docker-compose up --build
+if docker network create kbase-dev; then
+  echo "Docker network 'kbase-dev' created"
+else
+  echo "Docker network 'kbase-dev' already exists"
+fi
+
+if docker-compose up --build; then
+  echo "Docker Compose exited normally"
+else
+  echo "Docker Compose exited with error"
+fi
